a6a6a002966d111c3d5a9d2543788ba853d6df6b
Luiz Guilherme Crema <lgcrema@hotmail.com>
2024-07-31 14:48:51 -0300

    feature(controller): Implementacao Crud Pessoa e Endereco

--- api/app/Http/Controllers/EnderecoController.php
+++ api/app/Http/Controllers/EnderecoController.php
@@ -3,8 +3,48 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Endereco;
 
 class EnderecoController extends Controller
 {
     //
+    public function index(Request $request)
+    {
+        $query = Endereco::query();                                           //cria uma query para consulta da model imovel
+
+        $query->orderBy('id', 'asc');
+
+        return $query->paginate(5); 
+    }
+
+    public function store(Request $request)
+    {
+        $endereco = Endereco::create($request->all());
+
+        return response()
+            ->json($endereco, 201);
+    }
+
+    public function show (int $endereco)
+    {
+       $pessoaModel = Endereco::find($endereco);
+       if ($pessoaModel === null) {
+            return response()->json(['message' => 'Endereco nao encontrada'], 404);
+       }
+       return $pessoaModel;
+    }
+
+    public function update (Endereco $endereco, Request $request)
+    {
+        $endereco->fill($request->all());
+        $endereco->save();
+
+        return $endereco;
+    }
+
+    public function destroy (int $endereco) 
+    {
+        Endereco::destroy($endereco);
+        return response()->noContent();
+    }
 }
--- api/app/Http/Controllers/PessoaController.php
+++ api/app/Http/Controllers/PessoaController.php
@@ -2,9 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pessoa;
 use Illuminate\Http\Request;
 
 class PessoaController extends Controller
 {
     //
+    public function index(Request $request)
+    {
+        $query = Pessoa::query();
+        if ($request->has('nome')) {
+            $query->where('nome', $request->nome);
+        }
+
+        return $query->paginate(5);
+    }
+
+    public function store(Request $request)
+    {
+        $pessoa = Pessoa::create($request->all());
+
+        return response()
+            ->json($pessoa, 201);
+    }
+
+    public function show (int $pessoa)
+    {
+       $pessoaModel = Pessoa::find($pessoa);
+       if ($pessoaModel === null) {
+            return response()->json(['message' => 'Pessoa nao encontrada'], 404);
+       }
+       return $pessoaModel;
+    }
+
+    public function update (Pessoa $pessoa, Request $request)
+    {
+        $pessoa->fill($request->all());
+        $pessoa->save();
+
+        return $pessoa;
+    }
+
+    public function destroy (int $pessoa) 
+    {
+        Pessoa::destroy($pessoa);
+        return response()->noContent();
+    }
+
+
 }
